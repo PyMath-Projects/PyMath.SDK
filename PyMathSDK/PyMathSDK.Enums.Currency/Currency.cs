7240258fd6c89dea11ee9dfa8a4c853b1217e86a
Sea Oater <seaoater.pymath@gmail.com>
2023-07-21 21:44:07 +0200

    feat: Currency just seems to be the next enum to add.

---
+++ PyMathSDK/PyMathSDK.Enums.Currency/Currency.cs
@@ -0,0 +1,486 @@
+using System.ComponentModel;
+
+namespace PyMathSDK.Enums.Currency;
+
+public enum Currency
+{
+    [Description("United Arab Emirates dirham")]
+    AED = 1,
+
+    [Description("Afghan afghani")]
+    AFN = 2,
+
+    [Description("Albanian lek")]
+    ALL = 3,
+
+    [Description("Armenian dram")]
+    AMD = 4,
+
+    [Description("Netherlands Antillean guilder")]
+    ANG = 5,
+
+    [Description("Angolan kwanza")]
+    AOA = 6,
+
+    [Description("Argentine peso")]
+    ARS = 7,
+
+    [Description("Australian dollar")]
+    AUD = 8,
+
+    [Description("Aruban florin")]
+    AWG = 9,
+
+    [Description("Azerbaijani manat")]
+    AZN = 10,
+
+    [Description("Bosnia and Herzegovina convertible mark")]
+    BAM = 11,
+
+    [Description("Barbados dollar")]
+    BBD = 12,
+
+    [Description("Bangladeshi taka")]
+    BDT = 13,
+
+    [Description("Bulgarian lev")]
+    BGN = 14,
+
+    [Description("Bahraini dinar")]
+    BHD = 15,
+
+    [Description("Burundian franc")]
+    BIF = 16,
+
+    [Description("Bermudian dollar")]
+    BMD = 17,
+
+    [Description("Brunei dollar")]
+    BND = 18,
+
+    [Description("Boliviano")]
+    BOB = 19,
+
+    [Description("Brazilian real")]
+    BRL = 20,
+
+    [Description("Bahamian dollar")]
+    BSD = 21,
+
+    [Description("Bhutanese ngultrum")]
+    BTN = 22,
+
+    [Description("Botswana pula")]
+    BWP = 23,
+
+    [Description("Belarusian ruble")]
+    BYN = 24,
+
+    [Description("Belize dollar")]
+    BZD = 25,
+
+    [Description("Canadian dollar")]
+    CAD = 26,
+
+    [Description("Congolese franc")]
+    CDF = 27,
+
+    [Description("Swiss franc")]
+    CHF = 28,
+
+    [Description("Chilean peso")]
+    CLP = 29,
+
+    [Description("Renminbi (Chinese) yuan")]
+    CNY = 30,
+
+    [Description("Colombian peso")]
+    COP = 31,
+
+    [Description("Costa Rican colon")]
+    CRC = 32,
+
+    [Description("Cuban convertible peso")]
+    CUC = 33,
+
+    [Description("Cuban peso")]
+    CUP = 34,
+
+    [Description("Cape Verdean escudo")]
+    CVE = 35,
+
+    [Description("Czech koruna")]
+    CZK = 36,
+
+    [Description("Djiboutian franc")]
+    DJF = 37,
+
+    [Description("Danish krone")]
+    DKK = 38,
+
+    [Description("Dominican peso")]
+    DOP = 39,
+
+    [Description("Algerian dinar")]
+    DZD = 40,
+
+    [Description("Egyptian pound")]
+    EGP = 41,
+
+    [Description("Eritrean nakfa")]
+    ERN = 42,
+
+    [Description("Ethiopian birr")]
+    ETB = 43,
+
+    [Description("Euro")]
+    EUR = 44,
+
+    [Description("Fiji dollar")]
+    FJD = 45,
+
+    [Description("Falkland Islands pound")]
+    FKP = 46,
+
+    [Description("Pound sterling")]
+    GBP = 47,
+
+    [Description("Georgian lari")]
+    GEL = 48,
+
+    [Description("Guernsey pound")]
+    GGP = 49,
+
+    [Description("Ghanaian cedi")]
+    GHS = 50,
+
+    [Description("Gibraltar pound")]
+    GIP = 51,
+
+    [Description("Gambian dalasi")]
+    GMD = 52,
+
+    [Description("Guinean franc")]
+    GNF = 53,
+
+    [Description("Guatemalan quetzal")]
+    GTQ = 54,
+
+    [Description("Guyanese dollar")]
+    GYD = 55,
+
+    [Description("Hong Kong dollar")]
+    HKD = 56,
+
+    [Description("Honduran lempira")]
+    HNL = 57,
+
+    [Description("Croatian kuna")]
+    HRK = 58,
+
+    [Description("Haitian gourde")]
+    HTG = 59,
+
+    [Description("Hungarian forint")]
+    HUF = 60,
+
+    [Description("Indonesian rupiah")]
+    IDR = 61,
+
+    [Description("Israeli new shekel")]
+    ILS = 62,
+
+    [Description("Isle of Man pound")]
+    IMP = 63,
+
+    [Description("Indian rupee")]
+    INR = 64,
+
+    [Description("Iraqi dinar")]
+    IQD = 65,
+
+    [Description("Iranian rial")]
+    IRR = 66,
+
+    [Description("Icelandic króna")]
+    ISK = 67,
+
+    [Description("Jersey pound")]
+    JEP = 68,
+
+    [Description("Jamaican dollar")]
+    JMD = 69,
+
+    [Description("Jordanian dinar")]
+    JOD = 70,
+
+    [Description("Japanese yen")]
+    JPY = 71,
+
+    [Description("Kenyan shilling")]
+    KES = 72,
+
+    [Description("Kyrgyzstani som")]
+    KGS = 73,
+
+    [Description("Cambodian riel")]
+    KHR = 74,
+
+    [Description("Comorian franc")]
+    KMF = 75,
+
+    [Description("North Korean won")]
+    KPW = 76,
+
+    [Description("South Korean won")]
+    KRW = 77,
+
+    [Description("Kuwaiti dinar")]
+    KWD = 78,
+
+    [Description("Cayman Islands dollar")]
+    KYD = 79,
+
+    [Description("Kazakhstani tenge")]
+    KZT = 80,
+
+    [Description("Lao kip")]
+    LAK = 81,
+
+    [Description("Lebanese pound")]
+    LBP = 82,
+
+    [Description("Sri Lankan rupee")]
+    LKR = 83,
+
+    [Description("Liberian dollar")]
+    LRD = 84,
+
+    [Description("Lesotho loti")]
+    LSL = 85,
+
+    [Description("Libyan dinar")]
+    LYD = 86,
+
+    [Description("Moroccan dirham")]
+    MAD = 87,
+
+    [Description("Moldovan leu")]
+    MDL = 88,
+
+    [Description("Malagasy ariary")]
+    MGA = 89,
+
+    [Description("Macedonian denar")]
+    MKD = 90,
+
+    [Description("Burmese kyat")]
+    MMK = 91,
+
+    [Description("Mongolian tögrög")]
+    MNT = 92,
+
+    [Description("Macanese pataca")]
+    MOP = 93,
+
+    [Description("Mauritanian ouguiya")]
+    MRU = 94,
+
+    [Description("Mauritian rupee")]
+    MUR = 95,
+
+    [Description("Maldivian rufiyaa")]
+    MVR = 96,
+
+    [Description("Malawian kwacha")]
+    MWK = 97,
+
+    [Description("Mexican peso")]
+    MXN = 98,
+
+    [Description("Malaysian ringgit")]
+    MYR = 99,
+
+    [Description("Mozambican metical")]
+    MZN = 100,
+
+    [Description("Namibian dollar")]
+    NAD = 101,
+
+    [Description("Nigerian naira")]
+    NGN = 102,
+
+    [Description("Nicaraguan córdoba")]
+    NIO = 103,
+
+    [Description("Norwegian krone")]
+    NOK = 104,
+
+    [Description("Nepalese rupee")]
+    NPR = 105,
+
+    [Description("New Zealand dollar")]
+    NZD = 106,
+
+    [Description("Omani rial")]
+    OMR = 107,
+
+    [Description("Panamanian balboa")]
+    PAB = 108,
+
+    [Description("Peruvian sol")]
+    PEN = 109,
+
+    [Description("Papua New Guinean kina")]
+    PGK = 110,
+
+    [Description("Philippine peso")]
+    PHP = 111,
+
+    [Description("Pakistani rupee")]
+    PKR = 112,
+
+    [Description("Polish złoty")]
+    PLN = 113,
+
+    [Description("Paraguayan guaraní")]
+    PYG = 114,
+
+    [Description("Qatari riyal")]
+    QAR = 115,
+
+    [Description("Romanian leu")]
+    RON = 116,
+
+    [Description("Serbian dinar")]
+    RSD = 117,
+
+    [Description("Russian ruble")]
+    RUB = 118,
+
+    [Description("Rwandan franc")]
+    RWF = 119,
+
+    [Description("Saudi riyal")]
+    SAR = 120,
+
+    [Description("Solomon Islands dollar")]
+    SBD = 121,
+
+    [Description("Seychellois rupee")]
+    SCR = 122,
+
+    [Description("Sudanese pound")]
+    SDG = 123,
+
+    [Description("Swedish krona/kronor")]
+    SEK = 124,
+
+    [Description("Singapore dollar")]
+    SGD = 125,
+
+    [Description("Saint Helena pound")]
+    SHP = 126,
+
+    [Description("Sierra Leonean leone")]
+    SLL = 127,
+
+    [Description("Somali shilling")]
+    SOS = 128,
+
+    [Description("Surinamese dollar")]
+    SRD = 129,
+
+    [Description("South Sudanese pound")]
+    SSP = 130,
+
+    [Description("São Tomé and Príncipe dobra")]
+    STN = 131,
+
+    [Description("Salvadoran colón")]
+    SVC = 132,
+
+    [Description("Syrian pound")]
+    SYP = 133,
+
+    [Description("Swazi lilangeni")]
+    SZL = 134,
+
+    [Description("Thai baht")]
+    THB = 135,
+
+    [Description("Tajikistani somoni")]
+    TJS = 136,
+
+    [Description("Turkmenistan manat")]
+    TMT = 137,
+
+    [Description("Tunisian dinar")]
+    TND = 138,
+
+    [Description("Tongan paʻanga")]
+    TOP = 139,
+
+    [Description("Turkish lira")]
+    TRY = 140,
+
+    [Description("Trinidad and Tobago dollar")]
+    TTD = 141,
+
+    [Description("New Taiwan dollar")]
+    TWD = 142,
+
+    [Description("Tanzanian shilling")]
+    TZS = 143,
+
+    [Description("Ukrainian hryvnia")]
+    UAH = 144,
+
+    [Description("Ugandan shilling")]
+    UGX = 145,
+
+    [Description("United States dollar")]
+    USD = 146,
+
+    [Description("Uruguayan peso")]
+    UYU = 147,
+
+    [Description("Uzbekistani soʻm")]
+    UZS = 148,
+
+    [Description("Venezuelan bolívar soberano")]
+    VES = 149,
+
+    [Description("Vietnamese đồng")]
+    VND = 150,
+
+    [Description("Vanuatu vatu")]
+    VUV = 151,
+
+    [Description("Samoan tālā")]
+    WST = 152,
+
+    [Description("Central African CFA franc")]
+    XAF = 153,
+
+    [Description("Eastern Caribbean dollar")]
+    XCD = 154,
+
+    [Description("West African CFA franc")]
+    XOF = 155,
+
+    [Description("CFP franc (franc Pacifique)")]
+    XPF = 156,
+
+    [Description("Yemeni rial")]
+    YER = 157,
+
+    [Description("South African rand")]
+    ZAR = 158,
+
+    [Description("Zambian kwacha")]
+    ZMW = 159,
+
+    [Description("Zimbabwean dollar")]
+    ZWL = 160
+}
