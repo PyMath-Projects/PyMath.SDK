6d2afe6a58f3b185a59b338d22b4c4fee5296fa1
Sea Oater <seaoater.pymath@gmail.com>
2023-07-21 17:48:48 +0200

    init: Initial commit

---
+++ PyMathSDK/PyMathSDK.Enums/PyMathSDK.Enums.Country/Country.cs
@@ -0,0 +1,755 @@
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Enumerations;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public enum Country
+{
+    [Description("Afghanistan")]
+    AF = 1,
+
+    [Description("Åland Islands")]
+    AX = 2,
+
+    [Description("Albania")]
+    AL = 3,
+
+    [Description("Algeria")]
+    DZ = 4,
+
+    [Description("American Samoa")]
+    AS = 5,
+
+    [Description("Andorra")]
+    AD = 6,
+
+    [Description("Angola")]
+    AO = 7,
+
+    [Description("Anguilla")]
+    AI = 8,
+
+    [Description("Antarctica")]
+    AQ = 9,
+
+    [Description("Antigua and Barbuda")]
+    AG = 10,
+
+    [Description("Argentina")]
+    AR = 11,
+
+    [Description("Armenia")]
+    AM = 12,
+
+    [Description("Aruba")]
+    AW = 13,
+
+    [Description("Australia")]
+    AU = 14,
+
+    [Description("Austria")]
+    AT = 15,
+
+    [Description("Azerbaijan")]
+    AZ = 16,
+
+    [Description("Bahamas")]
+    BS = 17,
+
+    [Description("Bahrain")]
+    BH = 18,
+
+    [Description("Bangladesh")]
+    BD = 19,
+
+    [Description("Barbados")]
+    BB = 20,
+
+    [Description("Belarus")]
+    BY = 21,
+
+    [Description("Belgium")]
+    BE = 22,
+
+    [Description("Belize")]
+    BZ = 23,
+
+    [Description("Benin")]
+    BJ = 24,
+
+    [Description("Bermuda")]
+    BM = 25,
+
+    [Description("Bhutan")]
+    BT = 26,
+
+    [Description("Bolivia (Plurinational State of)")]
+    BO = 27,
+
+    [Description("Bonaire, Sint Eustatius and Saba")]
+    BQ = 28,
+
+    [Description("Bosnia and Herzegovina")]
+    BA = 29,
+
+    [Description("Botswana")]
+    BW = 30,
+
+    [Description("Bouvet Island")]
+    BV = 31,
+
+    [Description("Brazil")]
+    BR = 32,
+
+    [Description("British Indian Ocean Territory")]
+    IO = 33,
+
+    [Description("Brunei Darussalam")]
+    BN = 34,
+
+    [Description("Bulgaria")]
+    BG = 35,
+
+    [Description("Burkina Faso")]
+    BF = 36,
+
+    [Description("Burundi")]
+    BI = 37,
+
+    [Description("Cabo Verde")]
+    CV = 38,
+
+    [Description("Cambodia")]
+    KH = 39,
+
+    [Description("Cameroon")]
+    CM = 40,
+
+    [Description("Canada")]
+    CA = 41,
+
+    [Description("Cayman Islands")]
+    KY = 42,
+
+    [Description("Central African Republic")]
+    CF = 43,
+
+    [Description("Chad")]
+    TD = 44,
+
+    [Description("Chile")]
+    CL = 45,
+
+    [Description("China")]
+    CN = 46,
+
+    [Description("Christmas Island")]
+    CX = 47,
+
+    [Description("Cocos (Keeling) Islands")]
+    CC = 48,
+
+    [Description("Colombia")]
+    CO = 49,
+
+    [Description("Comoros")]
+    KM = 50,
+
+    [Description("Congo")]
+    CG = 51,
+
+    [Description("Congo (Democratic Republic of the)")]
+    CD = 52,
+
+    [Description("Cook Islands")]
+    CK = 53,
+
+    [Description("Costa Rica")]
+    CR = 54,
+
+    [Description("Côte d'Ivoire")]
+    CI = 55,
+
+    [Description("Croatia")]
+    HR = 56,
+
+    [Description("Cuba")]
+    CU = 57,
+
+    [Description("Curaçao")]
+    CW = 58,
+
+    [Description("Cyprus")]
+    CY = 59,
+
+    [Description("Czech Republic")]
+    CZ = 60,
+
+    [Description("Denmark")]
+    DK = 61,
+
+    [Description("Djibouti")]
+    DJ = 62,
+
+    [Description("Dominica")]
+    DM = 63,
+
+    [Description("Dominican Republic")]
+    DO = 64,
+
+    [Description("Ecuador")]
+    EC = 65,
+
+    [Description("Egypt")]
+    EG = 66,
+
+    [Description("El Salvador")]
+    SV = 67,
+
+    [Description("Equatorial Guinea")]
+    GQ = 68,
+
+    [Description("Eritrea")]
+    ER = 69,
+
+    [Description("Estonia")]
+    EE = 70,
+
+    [Description("Eswatini")]
+    SZ = 71,
+
+    [Description("Ethiopia")]
+    ET = 72,
+
+    [Description("Falkland Islands (Malvinas)")]
+    FK = 73,
+
+    [Description("Faroe Islands")]
+    FO = 74,
+
+    [Description("Fiji")]
+    FJ = 75,
+
+    [Description("Finland")]
+    FI = 76,
+
+    [Description("France")]
+    FR = 77,
+
+    [Description("French Guiana")]
+    GF = 78,
+
+    [Description("French Polynesia")]
+    PF = 79,
+
+    [Description("French Southern Territories")]
+    TF = 80,
+
+    [Description("Gabon")]
+    GA = 81,
+
+    [Description("Gambia")]
+    GM = 82,
+
+    [Description("Georgia")]
+    GE = 83,
+
+    [Description("Germany")]
+    DE = 84,
+
+    [Description("Ghana")]
+    GH = 85,
+
+    [Description("Gibraltar")]
+    GI = 86,
+
+    [Description("Greece")]
+    GR = 87,
+
+    [Description("Greenland")]
+    GL = 88,
+
+    [Description("Grenada")]
+    GD = 89,
+
+    [Description("Guadeloupe")]
+    GP = 90,
+
+    [Description("Guam")]
+    GU = 91,
+
+    [Description("Guatemala")]
+    GT = 92,
+
+    [Description("Guernsey")]
+    GG = 93,
+
+    [Description("Guinea")]
+    GN = 94,
+
+    [Description("Guinea-Bissau")]
+    GW = 95,
+
+    [Description("Guyana")]
+    GY = 96,
+
+    [Description("Haiti")]
+    HT = 97,
+
+    [Description("Heard Island and McDonald Islands")]
+    HM = 98,
+
+    [Description("Holy See")]
+    VA = 99,
+
+    [Description("Honduras")]
+    HN = 100,
+
+    [Description("Hong Kong")]
+    HK = 101,
+
+    [Description("Hungary")]
+    HU = 102,
+
+    [Description("Iceland")]
+    IS = 103,
+
+    [Description("India")]
+    IN = 104,
+
+    [Description("Indonesia")]
+    ID = 105,
+
+    [Description("Iran (Islamic Republic of)")]
+    IR = 106,
+
+    [Description("Iraq")]
+    IQ = 107,
+
+    [Description("Ireland")]
+    IE = 108,
+
+    [Description("Isle of Man")]
+    IM = 109,
+
+    [Description("Israel")]
+    IL = 110,
+
+    [Description("Italy")]
+    IT = 111,
+
+    [Description("Jamaica")]
+    JM = 112,
+
+    [Description("Japan")]
+    JP = 113,
+
+    [Description("Jersey")]
+    JE = 114,
+
+    [Description("Jordan")]
+    JO = 115,
+
+    [Description("Kazakhstan")]
+    KZ = 116,
+
+    [Description("Kenya")]
+    KE = 117,
+
+    [Description("Kiribati")]
+    KI = 118,
+
+    [Description("Korea (Democratic People's Republic of)")]
+    KP = 119,
+
+    [Description("Korea (Republic of)")]
+    KR = 120,
+
+    [Description("Kuwait")]
+    KW = 121,
+
+    [Description("Kyrgyzstan")]
+    KG = 122,
+
+    [Description("Lao People's Democratic Republic")]
+    LA = 123,
+
+    [Description("Latvia")]
+    LV = 124,
+
+    [Description("Lebanon")]
+    LB = 125,
+
+    [Description("Lesotho")]
+    LS = 126,
+
+    [Description("Liberia")]
+    LR = 127,
+
+    [Description("Libya")]
+    LY = 128,
+
+    [Description("Liechtenstein")]
+    LI = 129,
+
+    [Description("Lithuania")]
+    LT = 130,
+
+    [Description("Luxembourg")]
+    LU = 131,
+
+    [Description("Macao")]
+    MO = 132,
+
+    [Description("Madagascar")]
+    MG = 133,
+
+    [Description("Malawi")]
+    MW = 134,
+
+    [Description("Malaysia")]
+    MY = 135,
+
+    [Description("Maldives")]
+    MV = 136,
+
+    [Description("Mali")]
+    ML = 137,
+
+    [Description("Malta")]
+    MT = 138,
+
+    [Description("Marshall Islands")]
+    MH = 139,
+
+    [Description("Martinique")]
+    MQ = 140,
+
+    [Description("Mauritania")]
+    MR = 141,
+
+    [Description("Mauritius")]
+    MU = 142,
+
+    [Description("Mayotte")]
+    YT = 143,
+
+    [Description("Mexico")]
+    MX = 144,
+
+    [Description("Micronesia (Federated States of)")]
+    FM = 145,
+
+    [Description("Moldova (Republic of)")]
+    MD = 146,
+
+    [Description("Monaco")]
+    MC = 147,
+
+    [Description("Mongolia")]
+    MN = 148,
+
+    [Description("Montenegro")]
+    ME = 149,
+
+    [Description("Montserrat")]
+    MS = 150,
+
+    [Description("Morocco")]
+    MA = 151,
+
+    [Description("Mozambique")]
+    MZ = 152,
+
+    [Description("Myanmar")]
+    MM = 153,
+
+    [Description("Namibia")]
+    NA = 154,
+
+    [Description("Nauru")]
+    NR = 155,
+
+    [Description("Nepal")]
+    NP = 156,
+
+    [Description("Netherlands")]
+    NL = 157,
+
+    [Description("New Caledonia")]
+    NC = 158,
+
+    [Description("New Zealand")]
+    NZ = 159,
+
+    [Description("Nicaragua")]
+    NI = 160,
+
+    [Description("Niger")]
+    NE = 161,
+
+    [Description("Nigeria")]
+    NG = 162,
+
+    [Description("Niue")]
+    NU = 163,
+
+    [Description("Norfolk Island")]
+    NF = 164,
+
+    [Description("North Macedonia")]
+    MK = 165,
+
+    [Description("Northern Mariana Islands")]
+    MP = 166,
+
+    [Description("Norway")]
+    NO = 167,
+
+    [Description("Oman")]
+    OM = 168,
+
+    [Description("Pakistan")]
+    PK = 169,
+
+    [Description("Palau")]
+    PW = 170,
+
+    [Description("Palestine, State of")]
+    PS = 171,
+
+    [Description("Panama")]
+    PA = 172,
+
+    [Description("Papua New Guinea")]
+    PG = 173,
+
+    [Description("Paraguay")]
+    PY = 174,
+
+    [Description("Peru")]
+    PE = 175,
+
+    [Description("Philippines")]
+    PH = 176,
+
+    [Description("Pitcairn")]
+    PN = 177,
+
+    [Description("Poland")]
+    PL = 178,
+
+    [Description("Portugal")]
+    PT = 179,
+
+    [Description("Puerto Rico")]
+    PR = 180,
+
+    [Description("Qatar")]
+    QA = 181,
+
+    [Description("Réunion")]
+    RE = 182,
+
+    [Description("Romania")]
+    RO = 183,
+
+    [Description("Russian Federation")]
+    RU = 184,
+
+    [Description("Rwanda")]
+    RW = 185,
+
+    [Description("Saint Barthélemy")]
+    BL = 186,
+
+    [Description("Saint Helena, Ascension and Tristan da Cunha")]
+    SH = 187,
+
+    [Description("Saint Kitts and Nevis")]
+    KN = 188,
+
+    [Description("Saint Lucia")]
+    LC = 189,
+
+    [Description("Saint Martin (French part)")]
+    MF = 190,
+
+    [Description("Saint Pierre and Miquelon")]
+    PM = 191,
+
+    [Description("Saint Vincent and the Grenadines")]
+    VC = 192,
+
+    [Description("Samoa")]
+    WS = 193,
+
+    [Description("San Marino")]
+    SM = 194,
+
+    [Description("Sao Tome and Principe")]
+    ST = 195,
+
+    [Description("Saudi Arabia")]
+    SA = 196,
+
+    [Description("Senegal")]
+    SN = 197,
+
+    [Description("Serbia")]
+    RS = 198,
+
+    [Description("Seychelles")]
+    SC = 199,
+
+    [Description("Sierra Leone")]
+    SL = 200,
+
+    [Description("Singapore")]
+    SG = 201,
+
+    [Description("Sint Maarten (Dutch part)")]
+    SX = 202,
+
+    [Description("Slovakia")]
+    SK = 203,
+
+    [Description("Slovenia")]
+    SI = 204,
+
+    [Description("Solomon Islands")]
+    SB = 205,
+
+    [Description("Somalia")]
+    SO = 206,
+
+    [Description("South Africa")]
+    ZA = 207,
+
+    [Description("South Georgia and the South Sandwich Islands")]
+    GS = 208,
+
+    [Description("South Sudan")]
+    SS = 209,
+
+    [Description("Spain")]
+    ES = 210,
+
+    [Description("Sri Lanka")]
+    LK = 211,
+
+    [Description("Sudan")]
+    SD = 212,
+
+    [Description("Suriname")]
+    SR = 213,
+
+    [Description("Svalbard and Jan Mayen")]
+    SJ = 214,
+
+    [Description("Sweden")]
+    SE = 215,
+
+    [Description("Switzerland")]
+    CH = 216,
+
+    [Description("Syrian Arab Republic")]
+    SY = 217,
+
+    [Description("Taiwan, Province of China")]
+    TW = 218,
+
+    [Description("Tajikistan")]
+    TJ = 219,
+
+    [Description("Tanzania, United Republic of")]
+    TZ = 220,
+
+    [Description("Thailand")]
+    TH = 221,
+
+    [Description("Timor-Leste")]
+    TL = 222,
+
+    [Description("Togo")]
+    TG = 223,
+
+    [Description("Tokelau")]
+    TK = 224,
+
+    [Description("Tonga")]
+    TO = 225,
+
+    [Description("Trinidad and Tobago")]
+    TT = 226,
+
+    [Description("Tunisia")]
+    TN = 227,
+
+    [Description("Turkey")]
+    TR = 228,
+
+    [Description("Turkmenistan")]
+    TM = 229,
+
+    [Description("Turks and Caicos Islands")]
+    TC = 230,
+
+    [Description("Tuvalu")]
+    TV = 231,
+
+    [Description("Uganda")]
+    UG = 232,
+
+    [Description("Ukraine")]
+    UA = 233,
+
+    [Description("United Arab Emirates")]
+    AE = 234,
+
+    [Description("United Kingdom of Great Britain and Northern Ireland")]
+    GB = 235,
+
+    [Description("United States Minor Outlying Islands")]
+    UM = 236,
+
+    [Description("United States of America")]
+    US = 237,
+
+    [Description("Uruguay")]
+    UY = 238,
+
+    [Description("Uzbekistan")]
+    UZ = 239,
+
+    [Description("Vanuatu")]
+    VU = 240,
+
+    [Description("Venezuela (Bolivarian Republic of)")]
+    VE = 241,
+
+    [Description("Viet Nam")]
+    VN = 242,
+
+    [Description("Virgin Islands (British)")]
+    VG = 243,
+
+    [Description("Virgin Islands (U.S.)")]
+    VI = 244,
+
+    [Description("Wallis and Futuna")]
+    WF = 245,
+
+    [Description("Western Sahara")]
+    EH = 246,
+
+    [Description("Yemen")]
+    YE = 247,
+
+    [Description("Zambia")]
+    ZM = 248,
+
+    [Description("Zimbabwe")]
+    ZW = 249
+}
