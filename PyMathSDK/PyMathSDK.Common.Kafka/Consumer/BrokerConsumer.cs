4188dd55c9cfbdce327462c540d1823ca55cd3c3
Sea Oater <seaoater.pymath@gmail.com>
2023-07-27 20:52:01 +0200

    chore(Build): Remove build projects when building the solution. Signed-off-by: Sea Oater <seaoater.pymath@gmail.com>

--- PyMathSDK/PyMathSDK.Common.Kafka/Consumer/BrokerConsumer.cs
+++ PyMathSDK/PyMathSDK.Common.Kafka/Consumer/BrokerConsumer.cs
@@ -4,6 +4,13 @@ namespace PyMathSDK.Common.Kafka.Consumer;
 
 public class BrokerConsumer : IBrokerConsumer
 {
+    private readonly ConsumerConfig _config;
+
+    public BrokerConsumer(ConsumerConfig config)
+    {
+        _config = config;
+    }
+
     public Task<DeliveryResult<Null, string>> Consume(string topic, string message)
     {
         throw new NotImplementedException();
