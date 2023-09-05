f5ef9e5ef95f99e6df5789e5c825323eb2223444
Mduduzi <mduduzi.mlilo@hotmail.com>
2023-09-05 16:06:56 +0200

    chore(Eventing): Some refactors and modifications to eventing. Signed-off-by: Mduduzi <mduduzi.mlilo@hotmail.com>

--- PyMathSDK/PyMathSDK.Common.MassTransit/Eventing/EventBus/EventBus.cs
+++ PyMathSDK/PyMathSDK.Common.MassTransit/Eventing/EventBus/EventBus.cs
@@ -4,29 +4,25 @@ namespace PyMathSDK.Common.MassTransit.Eventing.EventBus;
 
 public class EventBus : IEventBus, IDisposable
 {
+    private readonly IPublishEndpoint _publishEndpoint;
     private readonly List<object> _messagesToPublish = new List<object>();
     public IPublishEndpoint Current { get; set; }
 
     public EventBus(IPublishEndpoint publishEndpoint)
     {
-        Current = publishEndpoint;
+        _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        Current = _publishEndpoint;
     }
-
-
-    public Task PublishAsync<T>(T message) where T : class
+    
+    public Task Publish<T>(T message) where T : class
     {
         _messagesToPublish.Add(message);
         return Task.CompletedTask;
     }
 
-    public void Publish<T>(T message) where T : class
-    {
-        _messagesToPublish.Add(message);
-    }
-
     public async Task FlushAllAsync(CancellationToken cancellationToken = default)
     {
-        await Current.PublishBatch(_messagesToPublish, cancellationToken);
+        await _publishEndpoint.PublishBatch(_messagesToPublish, cancellationToken);
         _messagesToPublish.Clear();
     }
 
--- PyMathSDK/PyMathSDK.Common.MassTransit/Eventing/EventBus/IEventBus.cs
+++ PyMathSDK/PyMathSDK.Common.MassTransit/Eventing/EventBus/IEventBus.cs
@@ -2,7 +2,5 @@ namespace PyMathSDK.Common.MassTransit.Eventing.EventBus;
 
 public interface IEventBus
 {
-    Task PublishAsync<T>(T message) where T : class;
-    
-    void Publish<T>(T message) where T : class;
+    Task Publish<T>(T message) where T : class;
 }
